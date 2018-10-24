92afcad3bc7c8c95ac1484e9a57a6f8a9c174fbe
CharlotteSophia7th <league@WorthyMini-5.attlocal.net>
2018-10-23 19:52:07 -0700

    Day #2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -1,6 +1,8 @@
 package section2;
 
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
@@ -8,6 +10,23 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+		Robot CSI=new Robot();
+		CSI.penDown();
+		CSI.setPenColor(Color.blue);
+		for (int i = 0; i < 12; i++) {
+			CSI.move(100);
+			CSI.turn(360/12);
+			
+		}
+		
+		
+		
+		
+
+		
+		
+		
+		
 	
 		
 		
