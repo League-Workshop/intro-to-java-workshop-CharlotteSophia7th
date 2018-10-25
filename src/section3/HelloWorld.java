7ddf78cc5b9a232bf0d0f68035d043194bff2ea0
CharlotteSophia7th <league@WorthyMini-5.attlocal.net>
2018-10-24 19:51:44 -0700

    Day #3

---
+++ src/section3/HelloWorld.java
@@ -0,0 +1,14 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+public static void main(String[] args) {
+	System.out.println("Hello World");
+	JOptionPane.showMessageDialog(null,"Hello World");
+	String name=JOptionPane.showInputDialog("Username?");
+	System.out.println(name);
+	JOptionPane.showMessageDialog(null,"Hello +name");
+	
+}
+}
